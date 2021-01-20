c8a03c8c19225325449d812f275ca75955889f47
zxcd7 <zxcd7@pop-os.localdomain>
2021-01-20 00:11:02 -0600

    Version 1.0

--- domotica/domotica.ino
+++ domotica/domotica.ino
@@ -11,12 +11,19 @@
 */
 #define FIREBASE_AUTH "ELBkqiVNrOHQS7pkfAEzRCFQJ8qgj9XI21prewjy"
 
+//Define FirebaseESP8266 data object
+FirebaseData fbdo;
+FirebaseData ledData;
+
+void printResult(FirebaseData &data);
+
 void setup() {
+
  pinMode (16,OUTPUT);
  pinMode (5,OUTPUT);
  pinMode (4,OUTPUT);
  pinMode (0,OUTPUT);
-  Serial.begin(2400);
+
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -29,34 +36,42 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  if (Firebase.getString("objects/obj1") == "1"){
+  if (Firebase.getString(ledData,"objects/obj1")){
+    if(ledData.stringData() == "1"){
     digitalWrite(16, 1);
-  }else{
+    }else{
     digitalWrite(16, 0);
+    }
   }
-  
-  if (Firebase.getString("objects/obj2") == "1"){
+
+  if (Firebase.getString(ledData,"objects/obj2")){
+    if(ledData.stringData() == "1"){
     digitalWrite(5, 1);
-  }else{
+    }else{
     digitalWrite(5, 0);
-  }
+    }
 
-  if (Firebase.getString("objects/obj3") == "1"){
+   if (Firebase.getString(ledData,"objects/obj3")){
+    if(ledData.stringData() == "1"){
     digitalWrite(4, 1);
-  }else{
+    }else{
     digitalWrite(4, 0);
+    }
   }
 
-  if (Firebase.getString("objects/obj3") == "1"){
+    if (Firebase.getString(ledData,"objects/obj4")){
+    if(ledData.stringData() == "1"){
     digitalWrite(0, 1);
-  }else{
+    }else{
     digitalWrite(0, 0);
+    }
   }
   
-  
   }
+}
 
   
