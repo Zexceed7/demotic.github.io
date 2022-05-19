d18c5e9c879a81e7d2604ef0f8e2879ed034d10f
Perez Gonzalez <amalgonz@amazon.com>
2022-05-19 03:42:18 -0500

    firebase sdk añadido

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,33 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+var firebaseConfig = {
+	apiKey: "AIzaSyAUURAIvtgGFPF1fzGhGJc23ldDSD-I8vg",  
+	authDomain: "rifa-amzn.firebaseapp.com",
+	databaseURL: "https://rifa-amzn-default-rtdb.firebaseio.com",
+	projectId: "rifa-amzn",
+	storageBucket: "rifa-amzn.appspot.com",
+	messagingSenderId: "36339791681",
+	appId: "1:36339791681:web:5a3af7337054fe329793c0"
+  };
+// Initialize Firebase
+var firebase;
+firebase.initializeApp(firebaseConfig);
+
+
+function post_blog(){
+    get_data()
+    var blog ={
+        text: document.getElementById("daddy-ticket").value
+    }
+    firebase.database().ref("Daddy/").set(blog)
+    document.getElementById("daddy-ticket").value="" 
+};
+
+
+
+
+
 (function($) {
 
 	var	$window = $(window),
